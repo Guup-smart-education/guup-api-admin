44f6ede287a79d15c5b035f0a4073019d746c552
Kelvin Arnold <kelvin.arnold.0030@gmail.com>
2020-12-30 00:32:20 -0300

    New version with service account keys

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -3,21 +3,21 @@ import firebaseAdmin from 'firebase-admin'
 
 const SERVICE_ACCOUNT = require('../../configs/firebase/serviceAccount/service-account-key.json')
 
-// const ADMIN_CONFIG = JSON.parse(`${process.env.FIREBASE_CONFIG}`)
-
-// console.log('=== ADMIN_CONFIG ===')
-// console.log(ADMIN_CONFIG)
-// console.log('=== ADMIN_CONFIG ===')
-
 if (!firebaseAdmin.apps.length) {
 	firebaseAdmin.initializeApp({
 		// credential: firebaseAdmin.credential.applicationDefault(),
 		credential: firebaseAdmin.credential.cert(SERVICE_ACCOUNT),
-		databaseURL: `'https://${process.env.GCLOUD_PROJECT}.firebaseio.com`,
+		databaseURL: `https://${process.env.GOOGLE_DATABASE_NAME}.firebaseio.com`,
 	})
 }
 console.log('=== FIREBASE APPS ===')
-console.log(firebaseAdmin.apps)
+console.log(
+	firebaseAdmin.apps.forEach((app, i, list) => {
+		if (app) {
+			console.log('APP: ', app)
+		}
+	})
+)
 console.log('=== FIREBASE APPS ===')
 
 export const db = firebaseAdmin.firestore()
