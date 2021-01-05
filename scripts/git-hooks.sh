2f9e94454eeea0432554d0ff509c7f2efdd96b59
Kelvin Arnold <kelvin.arnold.0030@gmail.com>
2021-01-05 20:59:03 -0300

    Updated git hooks

--- scripts/git-hooks.sh
+++ scripts/git-hooks.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo 'Setting develop project environment'
+echo 'Setting git hooks'
 echo 'START'
 echo '========'
-echo 'Copying git hooks to .git/hooks'
+echo 'Git hooks to .git/hooks'
 cp ./scripts/post-checkout .git/hooks
 chmod +x .git/hooks/post-checkout
 echo 'End copying git hooks'
